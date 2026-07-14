5d2e06e035a5923e4f94a9567742db620e58d9ae
mahfuzeee <45316693+mahfuzeee@users.noreply.github.com>
2026-07-14 23:38:03 +0600

    Core changed.

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,12 @@ const { errorHandler, notFound } = require("./middlewares/errorHandler");
 const routes = require("./routes/index");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 //api routes
